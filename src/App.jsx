fbabcc2979fc26c68ac5e687d96c87920a06a851
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-03-16 01:50:01 +0700

    feat(herosection): add hero section with diagonal split images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/NavbarSection'
+import HerroSection from './Components/HerroSection'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar/>
+      <HerroSection/>
 
     </>
   )
